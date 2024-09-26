d308ea2f409d2f0434cc09d6f42fd1263f77d24c
tbongkim03 <tbongkim03@gmial.com>
2024-09-26 11:09:37 +0900

    Update html

--- src/hnh/main.py
+++ src/hnh/main.py
@@ -1,17 +1,26 @@
 from typing import Union
-
+from fastapi import Request
 from fastapi import FastAPI, File, UploadFile
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/hello")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/")
+async def home(request: Request):
+    hotdog = "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQweb_7o7OrtlTP75oX2Q_keaoVYgAhMsYVp1sCafoNEdtSSaHps3n7NtNZwT_ufZGPyH7_9MFcao_r8QWr3Fdz17RitvZXLTU4dNsxr73m6V1scsH3_ZZHRw&usqp=CAE"
+    dog = "https://hearingsense.com.au/wp-content/uploads/2022/01/8-Fun-Facts-About-Your-Dog-s-Ears-1024x512.webp"
+    image_url = random.choice([hotdog, dog])
+    return html.TemplateResponse("index.html",{"request":request, "image_url": image_url})
+
 
 @app.post("/uploadfile/")
-def create_upload_file(file: UploadFile):
+def create_upload_file(file: UploadFile):    
     return {"file_name": file.filename}
 
 @app.get("/predict_hotdog")
